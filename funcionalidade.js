8bbe61c06dfe5795048445a278ca29d2dfaddb85
ThiagoCmrg <thiagosantoscamargo@hotmail.com>
2024-09-18 16:16:40 -0300

    Foi feita uma otimização das funcionalidades

--- funcionalidade.js
+++ funcionalidade.js
@@ -3,76 +3,41 @@ document.getElementById('from-currency-select').addEventListener('change', funct
   const paisSelecionado = this.value;
   const imgElement = document.getElementById('from-flag');
 
-  switch (paisSelecionado) {
-      case 'USD':
-          imgElement.src = 'https://flagsapi.com/US/flat/64.png'; 
-          break;
-      case 'BRL':
-          imgElement.src = 'https://flagsapi.com/BR/flat/64.png'; 
-          break;
-      case 'EUR':
-          imgElement.src = 'https://flagsapi.com/DE/flat/64.png'; 
-          break;
-      case 'JPY':
-          imgElement.src = 'https://flagsapi.com/JP/flat/64.png'; 
-          break;
-      case 'ZAR':
-          imgElement.src = 'https://flagsapi.com/ZA/flat/64.png'; 
-          break;
-      case 'AUD':
-          imgElement.src = 'https://flagsapi.com/AU/flat/64.png'; 
-          break;
-      case 'ARS':
-          imgElement.src = 'https://flagsapi.com/AR/flat/64.png'; 
-          break;
-      case 'CAD':
-          imgElement.src = 'https://flagsapi.com/CA/flat/64.png'; 
-          break;
-      default:
-          imgElement.src = ''; 
-          break;
-  }
+  const flags = {
+    'USD': 'https://flagsapi.com/US/flat/64.png',
+    'BRL': 'https://flagsapi.com/BR/flat/64.png',
+    'EUR': 'https://flagsapi.com/DE/flat/64.png',
+    'JPY': 'https://flagsapi.com/JP/flat/64.png',
+    'ZAR': 'https://flagsapi.com/ZA/flat/64.png',
+    'AUD': 'https://flagsapi.com/AU/flat/64.png',
+    'ARS': 'https://flagsapi.com/AR/flat/64.png',
+    'CAD': 'https://flagsapi.com/CA/flat/64.png',
+  };
+  imgElement.src = flags[paisSelecionado] || '';
 });
 
 document.getElementById('to-currency-select').addEventListener('change', function() {
   const paisSelecionado = this.value;
   const imgElement = document.getElementById('to-flag');
 
-  switch (paisSelecionado) {
-      case 'USD':
-          imgElement.src = 'https://flagsapi.com/US/flat/64.png'; 
-          break;
-      case 'BRL':
-          imgElement.src = 'https://flagsapi.com/BR/flat/64.png'; 
-          break;
-      case 'EUR':
-          imgElement.src = 'https://flagsapi.com/DE/flat/64.png'; 
-          break;
-      case 'JPY':
-          imgElement.src = 'https://flagsapi.com/JP/flat/64.png'; 
-          break;
-      case 'ZAR':
-          imgElement.src = 'https://flagsapi.com/ZA/flat/64.png'; 
-          break;
-      case 'AUD':
-          imgElement.src = 'https://flagsapi.com/AU/flat/64.png';
-          break;
-      case 'ARS':
-          imgElement.src = 'https://flagsapi.com/AR/flat/64.png'; 
-          break;
-      case 'CAD':
-          imgElement.src = 'https://flagsapi.com/CA/flat/64.png'; 
-          break;
-      default:
-          imgElement.src = ''; 
-          break;
-    }
+  const flags = {
+    'USD': 'https://flagsapi.com/US/flat/64.png',
+    'BRL': 'https://flagsapi.com/BR/flat/64.png',
+    'EUR': 'https://flagsapi.com/DE/flat/64.png',
+    'JPY': 'https://flagsapi.com/JP/flat/64.png',
+    'ZAR': 'https://flagsapi.com/ZA/flat/64.png',
+    'AUD': 'https://flagsapi.com/AU/flat/64.png',
+    'ARS': 'https://flagsapi.com/AR/flat/64.png',
+    'CAD': 'https://flagsapi.com/CA/flat/64.png',
+  };
+  imgElement.src = flags[paisSelecionado] || '';
 });
+
 
 //API DAS COTAÇÕES
 
 // Função para fazer a requisição e processar as cotações
-const obterCotacoes = function(callback) {
+const request = function(callback) {
   const moedas = 'USD-BRL,EUR-BRL,JPY-BRL,ZAR-BRL,AUD-BRL,ARS-BRL,CAD-BRL,BRL-USD,BRL-EUR,BRL-JPY,BRL-ZAR,BRL-AUD,BRL-ARS,BRL-CAD';
 
   fetch(`https://economia.awesomeapi.com.br/last/${moedas}`)
@@ -91,9 +56,8 @@ const obterCotacao = function(json, moeda) {
     'AUD': 'AUDBRL',
     'ARS': 'ARSBRL',
     'CAD': 'CADBRL',
-    'BRL': 'BRLUSD'
+    'BRL': 'BRLUSD',
   };
-  
   return json[chaves[moeda]] || null;
 };
 
@@ -104,7 +68,7 @@ document.getElementById('valor').addEventListener('keypress', function(e) {
     const deMoeda = document.getElementById('from-currency-select').value;
     const paraMoeda = document.getElementById('to-currency-select').value;
 
-    obterCotacoes(function(json) {
+    request(function(json) {
       converterMoedas(json, deMoeda, paraMoeda, valor);
     });
   }
@@ -122,12 +86,12 @@ const converterMoedas = function(json, deMoeda, paraMoeda, valor) {
 
     console.log(`${valor} ${deMoeda} = ${valorConvertido.toFixed(2)} ${paraMoeda}`);
 
-
+    
     // Atualizar a interface com o valor convertido
     document.getElementById('converted-value').innerText = ` ${valorConvertido.toFixed(2)}`;
 
 
-    // Exibir a data de atualização das cotações (em teste)
+    // Exibir a data de atualização das cotações
     const formattedDate = cotacaoDe.create_date;
     document.querySelector('.Atualizacao center').innerText = `Cotação atualizada em: ${formattedDate}`;
   } else {
